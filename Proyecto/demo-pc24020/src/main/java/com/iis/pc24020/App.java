a9a355e344e7ed57fe9e1f3ee3d3309b8ed7c862
PleitesGC-12 <primezero04@gmail.com>
2024-05-05 15:31:06 -0600

    Nuevo proyecto

---
+++ Proyecto/demo-pc24020/src/main/java/com/iis/pc24020/App.java
@@ -0,0 +1,18 @@
+package com.iis.pc24020;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{ 
+	public String Hello () {
+	return "Hello PC24020" ;
+}
+	
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println( hw.Hello() );
+    }
+}
